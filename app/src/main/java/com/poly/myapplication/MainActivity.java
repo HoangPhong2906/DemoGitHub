42b80c0a61fe8711ced22d4087d2bc10b951cc8d
KingWind2906 <94579182+KingWind2906@users.noreply.github.com>
2021-11-18 09:20:44 +0700

    test LẦN 1

--- app/src/main/java/com/poly/myapplication/MainActivity.java
+++ app/src/main/java/com/poly/myapplication/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //hello
         System.out.println("Hello");
+        String test ="ĐÂY LÀ pHONG ĐẸP TRAI";
     }
 }
